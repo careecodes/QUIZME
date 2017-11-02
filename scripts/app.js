74b99eaa20541e4c8a6b642a25d3f882711d6379
caree claunch <caree@caree.codes>
2017-11-02 08:40:53 -0500

    Added some helpful comments so I don't forget what happened

--- scripts/app.js
+++ scripts/app.js
@@ -13,17 +13,28 @@ const appViewModel = new Vue({
         }
     },
 
+    
+    // once the view is mounted, do these things
     mounted() {
         this.getData()
     },
 
     methods: {
         getData() {  
-            var self = this;          
+            // This will preserve the 'this' context prior to the function below. An arrow function also preserves 'this' context.
+            // Is there a better way? TODO convert to arrow function?
+            var self = this;
+
+            // Now let's get that API response
             fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple")
             .then(response=> response.json())
             .then((json) => {
+                // assisn the first question's category to the category property -> where is this going? How does actually GET into data() above? 
                 this.category = json.results[0].category;
+
+                // We must loop through the results to grab the question property of each object in the array
+                // Then write to our own array of questions
+                // Is there a better way to do this?!?!?!
                 json.results.forEach(function(element, i){
                     self.questions.push(element.question)
                     console.log(element, i);
